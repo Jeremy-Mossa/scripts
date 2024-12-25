3b2fddff639e1618f110821d8509be28ac372dd7
Jeremy Mossa <>
2024-12-25 12:00:00 -0500

    Automated commit by using crontab

--- git-autopush.sh
+++ git-autopush.sh
@@ -5,7 +5,7 @@ git_dirs=$(find $HOME -type d -name ".git" -prune -exec dirname {} \;)
 for dir in $git_dirs; do
 	cd "$dir" || continue
 	git add .
-	git commit -m "Automated commit from systemd service" || echo "No \
+	git commit -m "Automated commit by using crontab" || echo "No \
 		changes to commit in $dir."
 	git push || echo "Failed to push changes in $dir." 
 done
