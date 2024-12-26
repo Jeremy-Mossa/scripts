5795cf184368eb99b08f3c86e9cbc69f2079d984
Jeremy Mossa <>
2024-12-25 21:00:01 -0500

    Automated commit by using crontab

--- dotfile-backups.sh
+++ dotfile-backups.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-:
+Dir="/home/fedora41/dotfiles"
+
+df=(
+  ".bashrc"
+  ".bash_profile"
+  ".dillo/"
+  ".fluxbox/"
+  ".vimrc"
+  ".xinitrc"
+  ".Xresources"
+)
+
+cd ~
+pwd
+
+for f in "${df[@]}"; do
+  if [ -f "$f" ]; then
+    cp -r $f $Dir
+    echo $f
+  else
+    echo "File not found: $file"
+  fi
+done
+
+cd "$Dir" || exit
+git add .
+git commit -m "Scheduled backup of dotfiles" || echo "no changes" 
+git push || echo "git push failure"
