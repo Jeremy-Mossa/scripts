5a562444cd60fe6838fa4bb188480962be75e27b
Jeremy Mossa <>
2024-12-27 04:00:00 -0500

    Automated commit by using crontab

--- chroot_jail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# How to make a chroot jail
-
-sudo mkdir /mnt/chroot
-sudo mount --bind /dev /mnt/chroot/dev
-sudo mount --bind /proc /mnt/chroot/proc
-sudo mount --bind /sys /mnt/chroot/sys
-sudo mount --bind /etc/resolv.conf /mnt/chroot/etc/resolv.conf
