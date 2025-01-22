218cad93167c909efb1c651ce1e7d5a3c9edb502
Jeremy-Mossa <jeremy.mossa@gmail.com>
2025-01-22 14:49:24 -0800

    Automated commit by using crontab

--- git-autopush.sh
+++ git-autopush.sh
@@ -4,10 +4,10 @@ git_dirs=$(find $HOME -type d -name ".git" -prune -exec dirname {} \;)
 
 for dir in $git_dirs; do
 	cd "$dir" || continue
+  # Pull first in case editing was done on github
+  git pull ssh main
 	git add .
 	git commit -m "Automated commit by using crontab" || echo "No \
 		changes to commit in $dir."
 	git push ssh main || echo "Failed to push changes in $dir." 
-    # Pull first in case editing was done on github
-    git pull ssh main
 done
