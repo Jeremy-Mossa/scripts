a958777e2d811391c85fb725a1d856b50f99d0af
blank <you@example.com>
2025-02-27 11:30:03 -0800

    Automated commit by using crontab

---
+++ ticker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gnucash-cli --quotes dump yahooweb \
+  wolf 2>/dev/null > wolf.tkr
+
+price=$(grep last wolf.tkr \
+  | cut -d':' -f2 \
+  | cut -d'<' -f1)
+printf "\tWOLF: $price\n" | lolcat
+
+rm wolf.tkr
