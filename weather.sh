976d0390a50eb9f09adabadf2b00ff19ea93216f
blank <you@example.com>
2025-02-27 18:00:02 -0800

    Automated commit by using crontab

--- weather.sh
+++ weather.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 city=$1
-curl -o forecast https://www.timeanddate.com/weather/usa/$city/ext
+country=$2
+curl -o forecast https://www.timeanddate.com/weather/$country/$city/ext
 cat forecast | grep \"date\" > wthr
 mv wthr forecast
