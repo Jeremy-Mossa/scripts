d444ce42f18c37754e42413ebf72b5d95c55257f
blank <you@example.com>
2025-02-27 16:30:02 -0800

    Automated commit by using crontab

--- weather.pl
+++ weather.pl
@@ -35,7 +35,42 @@ my $content = do { local $/; <$fh> };
 close $fh;
 
 # Extract and print all content between "date" and "}"
-while ($content =~ /"date"(.*?)}/sg) {
-    my $extracted_content = $1;
-    print "$extracted_content\n";
+# while ($content =~ /"date"(.*?)}/sg) {
+#     my $extracted_content = $1;
+#     print "$extracted_content\n\n";
+# }
+
+# Define a hash map for weather description to Unicode glyphs
+my %weather_glyphs = (
+    "Sunny"          => "🌞🌞🌞",
+    "Partly cloudy"  => "\x{26C5}",
+    "Cloudy"         => "\x{2601}",
+    "Light rain"     => "\x{1F327}",
+);
+
+# Helper function to convert date to ordinal
+sub ordinal {
+    my $n = shift;
+    return $n . 'th' if $n =~ /1[0-9]$/;
+    return $n . 'st' if $n =~ /1$/;
+    return $n . 'nd' if $n =~ /2$/;
+    return $n . 'rd' if $n =~ /3$/;
+    return $n . 'th';
 }
+
+# Extract and print all content between "date" and "}"
+while ($content =~ /"ts":"(.*?)","ds":"(.*?)","icon":\d+,"desc":"(.*?)","temp":(\d+),"templow":(\d+),"cf":\d+,"wind":\d+,"wd":\d+,"hum":\d+,"pc":\d+(?:,"rain":\d+.\d+)?/sg) {
+    my ($short_date, $long_date, $desc, $temp, $templow) = ($1, $2, $3, $4, $5);
+
+    # Get the Unicode glyph for the weather description
+    my $glyph = $weather_glyphs{$desc} // "";
+
+    # Convert the long date to the required format
+    $long_date =~ /^(\w+), (\w+) (\d+)/;
+    my $formatted_date = "$1 " . ordinal($3);
+
+    # Print the formatted output
+    printf "%-10s%s%-8s Low: %-2d High: %-2d\n", $formatted_date, $glyph, $desc, $templow, $temp;
+}
+
+print "\x{1F327}"x5, "\n";
