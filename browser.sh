1e9c1766ec4a8a49fbcbd49b9fe1ae7dbdf334e8
blank <you@example.com>
2025-03-01 20:00:13 -0800

    Automated commit by using crontab

--- browser.sh
+++ browser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 librewolf >/dev/null 2>/dev/null &
-sleep 1
+sleep 1.5
 browser_window=$(xdotool search librewolf | tail -n1)
 xdotool windowsize $browser_window 68% 94%
 xterm -geometry 60x53
--- weather.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-city=$1
-country=$2
-curl -o forecast https://www.timeanddate.com/weather/$country/$city/ext
-cat forecast | grep \"date\" > wthr
-mv wthr forecast
