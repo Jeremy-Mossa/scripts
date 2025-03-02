5dc66ead32f68def3c299f0c4da06a9fcdab44b3
blank <you@example.com>
2025-03-01 23:00:28 -0800

    Automated commit by using crontab

--- wthr.pl
+++ wthr.pl
@@ -28,15 +28,54 @@ else
 my $points_url   = "https://api.weather.gov/points/$lat,$lon";
 my $api_response = `curl -s "$points_url"`;
 
-# $points_url only gives further urls in a json
+# $points_url only gives further urls in a JSON
 # we need to find and store the forecast url
-if ($api_response =~ /"forecast":"([^"]+)"/)
+my $forecast_url;
+if ($api_response =~ /"forecast": "([^"]+)"/)
 {
-  my $forecast_url = $1;
+  $forecast_url = $1;
 }
 else
 {
   die "Failed to extract forecast URL.\n";
 }
 
-my $weather = `curl -s "$forecast_url"`;
+# save the forecast JSON response
+my $forecast = `curl -s "$forecast_url"`;
+
+my @labels = qw(
+  name
+  startTime
+  temperature
+  temperatureUnit
+  value
+  shortForecast
+);
+
+foreach my $label (@labels)
+{
+  say $label;
+}
+
+
+# "periods": [
+#            {
+#                "number": 1,
+#                "name": "Overnight",
+#                "startTime": "2025-03-02T00:00:00-06:00",
+#                "endTime": "2025-03-02T06:00:00-06:00",
+#                "isDaytime": false,
+#                "temperature": 56,
+#                "temperatureUnit": "F",
+#                "temperatureTrend": "",
+#                "probabilityOfPrecipitation": {
+#                    "unitCode": "wmoUnit:percent",
+#                    "value": null
+#                },
+#                "windSpeed": "5 mph",
+#                "windDirection": "ESE",
+#                "icon": "https://api.weather.gov/icons/land/night/few?size=medium",
+#                "shortForecast": "Mostly Clear",
+#                "detailedForecast": "Mostly clear, with a low around 56. East southeast wind around 5 mph."
+#            },
+#
