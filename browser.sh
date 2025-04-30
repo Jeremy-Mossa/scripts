c5597a1739c88d72629328140e79d05c4734db99
blank <you@example.com>
2025-04-30 02:30:06 -0700

    Automated commit by using crontab

--- browser.sh
+++ browser.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-mullvad-browser https://grok.com >/dev/null 2>/dev/null &
-sleep 2.5
-browser_window=$(xdotool search mullvad \
-   | tail -n1)
+mullvad-browser https://www.meta.ai \
+  >/dev/null 2>/dev/null &
+sleep 2
+
+browser_window=$(xdotool search --name \
+   "Mullvad" | tail -n1)
 xdotool windowsize $browser_window 68% 94%
 xterm -geometry 60x53
+
+
+xdotool mousemove 517 462 
+sleep 8
+xdotool click 1
+sleep 1
+xdotool type "hey"
+sleep 0.25
+
+xdotool mousemove 948 540 click 1
+sleep 0.25
+xdotool mousemove 852 664 click 1
+sleep 0.25
+xdotool mousemove 760 737 
+sleep 0.25
+
+xdotool click --repeat 10 5
+sleep 0.25
+xdotool click 1
+sleep 0.25
+xdotool enter Return
+sleep 0.25
+xdotool click 1
+sleep 0.25
