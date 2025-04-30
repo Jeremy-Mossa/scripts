029969431e90b8164cd87b084c2cef7f59df4669
blank <you@example.com>
2025-04-29 17:30:00 -0700

    Automated commit by using crontab

--- android.sh
+++ android.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 
-# Unlock android device
-adb shell input keyevent 82
+if $(adb shell dumpsys window | \
+  grep -q 'Lockscreen=true'); then
+ 
+  # Unlock android device
+  adb shell input keyevent 82
+  sleep 0.25
+  adb shell input swipe 300 1000 300 500
+  sleep 0.25
+  adb shell input text "071981"
+fi
+
+if $(adb shell pidof com.kabam.marvelbattle); then
+  echo "MCoC already running"
+
+  else
+    # Start MCoC
+    adb shell monkey -p com.kabam.marvelbattle 1
+fi
