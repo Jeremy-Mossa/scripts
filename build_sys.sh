aff837f970dfd3b3e7bdcd40e1f79f9c4dc26e25
blank <you@example.com>
2025-05-20 12:00:07 +0700

    Automated commit by using crontab

--- build_sys.sh
+++ build_sys.sh
@@ -1,59 +1,61 @@
-#!/bin/bash
-
-# Elevate privileges before install
-sudo -v
-
-rm -r ~/Desktop ~/Videos ~/Music \
-  ~/Templates ~/Pictures 2>/dev/null
-
-if [ ! -d ~/books ]; then
-  mkdir ~/books
-fi
-
-if [ ! -d ~/C ]; then
-  mkdir ~/C
-fi
-
-if [ ! -d ~/scripts ]; then
-  git clone https://github.com/Jeremy-Mossa/scripts
-fi
-
-if [ ! -d ~/dotfiles ]; then
-  git clone https://github.com/Jeremy-Mossa/dotfiles
-fi
-
-if [ ! -n ~/downloads ]; then
-  ln -s ~/Downloads downloads
-fi
-
-if [ ! -n ~/perl ]; then
-  mkdir ~/perl
-fi
-
-for line in $(cat ~/.dependency); do
-  yes | sudo dnf install $line
-done
-
-for f in $(find / -iname *gnome* 2>/dev/null); do 
-  rm -r $f
-done
-
-gotslim=$(which slim)
-gdmstatus=$(systemctl status gdm | grep disabled | cut -d";" -f2)
-if [ $gotslim = "/bin/slim" ] && [ $gdmstatus != "disabled" ]; then 
-  sudo systemctl disable gdm 2>/dev/null
-  sudo systemctl enable slim 2>/dev/null
-else
-  echo "gdm status: $gdmstatus"
-fi
-
-# main.zip is ~22MB
-if [ ! -d ~/Storage ]; then
-  wget -O storage.zip \
-  https://github.com/Jeremy-Mossa/Storage/archive/main.zip
-  unzip storage.zip
-  rm storage.zip
-  mv Storage-main ~/Storage
-fi
-
-echo '@reboot root echo 85 > /sys/class/power_supply/BAT0/charge_control_end_threshold' >> /etc/crontab
+aalib
+acpi
+adb 
+boxes
+bubblewrap
+cronie
+dillo
+dkms
+fastboot
+fastfetch
+feh
+figlet
+firefox-esr
+firejail
+fluxbox
+fzf
+gcc
+giflib
+git
+gnucash
+htop
+icecat
+libcgif
+libjpeg
+libpng
+lolcat
+mullvad-browser
+mullvad-vpn
+mupdf
+mutt
+net-tools
+perl-Gtk3
+perl-HTTP-Tiny
+perl-JSON-PP
+prename
+redshift
+rtorrent
+slim
+source-foundry-hack-fonts
+ssh
+ssh-keygen
+startx
+texlive
+tldr
+tmux
+toilet
+vim
+vimb
+vlc
+xclip
+xdotool
+xlockmore
+xorg
+xorg-x11-font-utils
+xorg-x11-server-Xorg
+xorg-x11-server-Xvfb
+xrdb
+xsecurelock
+xterm
+xz-lzma-compat
+yt-dlp
