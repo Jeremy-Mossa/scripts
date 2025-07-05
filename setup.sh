6e7fd3ed412edb6a6540caccc1f49eb0d45ebfe2
blank <you@example.com>
2025-07-05 11:00:04 -0700

    Automated commit by using crontab

--- setup.sh
+++ setup.sh
@@ -147,19 +147,35 @@ done
 # Add to the render group
 usermod -aG render jbm
 
-# Define a udev rule to detect android devices
-UDEV_RULE='SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ACTION=="add", RUN+="/usr/local/bin/android-detect.sh"'
+# Define the udev rules for all major phone/tablet vendors
+UDEV_RULES='SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="12d1", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="2717", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="22d9", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="2d95", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="2a70", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="1004", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="054c", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="0bb4", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="0b05", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="17ef", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="19d2", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="1949", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"
+SUBSYSTEM=="usb", ATTR{idVendor}=="0489", ACTION=="add", RUN+="/usr/sbin/runuser -u jbm -- /home/jbm/scripts/android-detect.sh"'
+
 UDEV_FILE="/etc/udev/rules.d/99-android-detect.rules"
 
-# Check if the rule already exists in the file
-if [ -f "$UDEV_FILE" ] && grep -Fx "$UDEV_RULE" "$UDEV_FILE" >/dev/null; then
-    echo "udev rule already exists, skipping..."
+# Check if the rules already exist in the file
+if [ -f "$UDEV_FILE" ] && echo "$UDEV_RULES" | grep -Fx -f - "$UDEV_FILE" >/dev/null; then
+    echo "udev rules already exist, skipping..."
 else
-    # Echo the rule to the file
-    echo "$UDEV_RULE" | tee "$UDEV_FILE" >/dev/null
+    # Echo the rules to the file
+    echo "$UDEV_RULES" | tee "$UDEV_FILE" >/dev/null
     # Set correct permissions
     chmod 644 "$UDEV_FILE"
-    echo "udev rule added."
+    echo "udev rules added."
 fi
 
 # Reload udev rules
