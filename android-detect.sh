7f59249e4aad7fb57ae79c3ecb07de3a8ceae9ac
blank <you@example.com>
2025-07-05 11:30:04 -0700

    Automated commit by using crontab

--- android-detect.sh
+++ android-detect.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-DISPLAY=:0 xterm -e "echo 'Android device found'; sleep 5"
+# DISPLAY=:0 xterm -e "echo 'Android device found'; sleep 5"
+xterm -e "echo 'Android device found'; sleep 5"
