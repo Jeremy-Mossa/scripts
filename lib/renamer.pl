81046dcb3f8b2ff1160b3de08be5e1c248396d9f
none <jeremy.mossa@gmail.com>
2025-12-27 15:00:01 -0800

    Auto-commit — 2025-12-27 15:00:01

---
+++ lib/renamer.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.40.0; # version of perl
+use utf8;
+use autodie; # to handle errors
+use Data::Dumper;
+
+# make Data::Dumper pretty-print
+$Data::Dumper::Terse = 1; # just dump data contents
+$Data::Dumper::Indent = 0; # single-line output
+
+
