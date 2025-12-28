e8527e4b43477d011f202a163a995c9a0133e349
none <jeremy.mossa@gmail.com>
2025-12-27 16:00:02 -0800

    Auto-commit — 2025-12-27 16:00:02

--- lib/renamer.pl
+++ lib/renamer.pl
@@ -5,10 +5,48 @@ use warnings;
 use 5.40.0; # version of perl
 use utf8;
 use autodie; # to handle errors
-use Data::Dumper;
+use Cwd qw(getcwd);
 
-# make Data::Dumper pretty-print
-$Data::Dumper::Terse = 1; # just dump data contents
-$Data::Dumper::Indent = 0; # single-line output
 
+sub clean_filenames() {
+  my $current_dir = getcwd();
+
+  opendir( my $dh, $current_dir );
+  while ( my $file = readdir($dh) ) {
+    if ( $file =~ /^\./ )            { next; }
+    if ( $file !~ /\.(opus|mp4)$/i ) { next; }
+
+    my $new_name = $file;
+
+    $new_name =~ s/'s/s/g;
+    $new_name =~ s/\[[^]]*\]//g;
+    $new_name =~ s/ \./\./g;
+    $new_name =~ s/ +/ /g;
+    $new_name =~ s/ - / /g;
+    $new_name =~ s/- / /g;
+    $new_name =~ s/ -/ /g;
+    $new_name =~ s/, / /g;
+    $new_name =~ s/[()]//g;
+    $new_name =~ s/： /_/g;
+    $new_name =~ s/ ｜ / /g;
+    $new_name =~ s/\.{3}//g;
+    $new_name =~ s/\. \. \. //g;
+    $new_name =~ s/ \. \. \. //g;
+    $new_name =~ s/\.\.\.opus/\.opus/g;
+    $new_name =~ s/\.\.opus/\.opus/g;
+    $new_name =~ s/\.{2}/\./g;
+    $new_name =~ s/ \/\///g;
+    $new_name =~ s/ ⧸⧸//g;
+    $new_name =~ s/!!//g;
+    $new_name =~ s/\s+(?=\.m)/ /g;
+    $new_name =~ s/([^.])opus$/$1.opus/i;
+    $new_name =~ s/([^.])mp4$/$1.mp4/i;
+    $new_name =~ s/^\s+|\s+$//g;
+
+    if ( $new_name ne $file ) {
+      rename( "$current_dir/$file", "$current_dir/$new_name" )
+        or warn "Rename failed: $file → $new_name : $!\n";
+    }
+  }
+closedir($dh);
 
--- ytdlp.pl
+++ ytdlp.pl
@@ -8,6 +8,7 @@ use utf8;
 use autodie;
 use File::Basename;
 use File::Spec;
+require "$ENV{HOME}/scripts/lib/clean_filenames.pl";
 
 
 # ----------------------------- CONFIGURATION -----------------------------
@@ -41,51 +42,6 @@ while ( my $line = <$fh_ignore> ) {
     $ignored_urls{$line} = 1;
 }
 close $fh_ignore;
-
-# --------------------------- CLEANER FUNCTION ---------------------------
-sub clean_filenames() {
-    my $current_dir = $ENV{PWD};
-
-    opendir( my $dh, $current_dir );
-    while ( my $file = readdir($dh) ) {
-        if ( $file =~ /^\./ )            { next; }    # skip hidden
-        if ( $file !~ /\.(opus|mp4)$/i ) { next; }
-
-        my $new_name = $file;
-
-        $new_name =~ s/'s/s/g;
-        $new_name =~ s/\[[^]]*\]//g;
-        $new_name =~ s/ \./\./g;
-        $new_name =~ s/ +/ /g;
-        $new_name =~ s/ - / /g;
-        $new_name =~ s/- / /g;
-        $new_name =~ s/ -/ /g;
-        $new_name =~ s/, / /g;
-        $new_name =~ s/[()]//g;
-        $new_name =~ s/： /_/g;
-        $new_name =~ s/ ｜ / /g;
-        $new_name =~ s/\.{3}//g;
-        $new_name =~ s/\. \. \. //g;
-        $new_name =~ s/ \. \. \. //g;
-        $new_name =~ s/\.\.\.opus/\.opus/g;
-        $new_name =~ s/\.\.opus/\.opus/g;
-        $new_name =~ s/\.{2}/\./g;
-        $new_name =~ s/ \/\///g;
-        $new_name =~ s/ ⧸⧸//g;
-        $new_name =~ s/!!//g;
-        $new_name =~ s/\s+(?=\.m)/ /g;
-        $new_name =~ s/([^.])opus$/$1.opus/i;
-        $new_name =~ s/([^.])mp4$/$1.mp4/i;
-        $new_name =~ s/^\s+|\s+$//g;            # trim
-
-        if ( $new_name ne $file ) {
-            rename( "$current_dir/$file", "$current_dir/$new_name" )
-              or warn "Rename failed: $file → $new_name : $!\n";
-        }
-    }
-    closedir($dh);
-}
-# ------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------
 # No argument → download new audio from all channels in channels.txt
