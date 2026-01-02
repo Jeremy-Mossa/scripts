75deea7dce9c37c41990815106efae9b36a6171d
none <jeremy.mossa@gmail.com>
2026-01-01 17:30:02 -0800

    Auto-commit — 2026-01-01 17:30:02

--- progperl.pl
+++ progperl.pl
@@ -46,9 +46,7 @@ open(my $fh, '>', $file_name);
 print $fh <<'EOF';
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use 5.40.0; # version of perl
+use 5.42.0; # version of perl
 use utf8;
 use autodie; # to handle errors
 use Data::Dumper;
