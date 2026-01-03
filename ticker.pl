1f02ca57c2407f2606ccd25f7c57860219d4baaa
none <jeremy.mossa@gmail.com>
2026-01-03 10:30:02 -0800

    Auto-commit — 2026-01-03 10:30:02

--- ticker.pl
+++ ticker.pl
@@ -1,134 +1,124 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-use 5.40.0;
+
+use 5.42.0; # version of perl
 use utf8;
-use autodie;
-use Mojolicious::Lite;
+use autodie; # to handle errors
+use Mojo::DOM;
+use Mojo::File;  # For reading files easily
+
+
+#--------------------------- Internet check ----------------------------
+
+# idle until connected to internet
+while (1) {
+  if (system('ping -c 1 ddg.gg >/dev/null') != 0) {
+    sleep 60;
+  }
+  else { last; }
+}
+
+#-----------------------------------------------------------------------
+
+
+#------------------------------- Config --------------------------------
 
 # Orange ANSI color code
 my $orange = "\e[38;5;208m";
 my $reset  = "\e[0m";
 
-my @tickers = (
-    "DIVO",
-    "BITO",
-    "SVOL",
-    "JEPQ",
-    "MAIN",
-    "GAIN",
-    "HRZN",
-    "PFLT",
-    "OXLC",
-    "DX"
-);
-
+# cache results for instant lookup later
 my $cache_file = "/home/jbm/Documents/ticker_cache";
 
-# Load cache if exists
-my %cache;
-if (-f $cache_file) {
-    open my $fh, '<', $cache_file;
-    while (my $line = <$fh>) {
-        chomp $line;
-        my ($tick, $pr) = split /:/, $line;
-        $cache{$tick} = $pr if $tick && $pr;
-    }
-    close $fh;
+my $cache = '';
+my $url = 'https://stooq.com/q/?s=';
+
+# tickers for stooq.com queries
+my @tickers = (
+  'bito',
+  'divo',
+  'dx',
+  'gain',
+  'hrzn',
+  'jepq',
+  'main',
+  'oxlc',
+  'pflt',
+  'svol'
+);
+
+#-----------------------------------------------------------------------
+
+
+#----------------------- Functions (subroutines) -----------------------
+
+sub cache_prices($ticker) {
+
+  # Capture output from curl
+  system("curl --silent -o $ticker.txt $url$ticker.us");
+
+  # Load HTML from text file
+  my $html = Mojo::File->new("$ticker.txt")->slurp;
+  unlink "$ticker.txt";
+
+  # Parse the HTML
+  my $dom   = Mojo::DOM->new($html);
+  my $price = $dom->at('span#aq_' . $ticker . '\.us_c4');
+  $price    = $price->text;
+  
+  # truncate fractional pennies, left-pad price
+  $price = sprintf("%.2f", $price);
+  $price = sprintf("%6s", $price);
+
+  # right-pad ticker: e.g. 'main' and 'dx  ' 
+  $ticker = sprintf("%-4s", $ticker);
+
+  if ($price) {
+      $cache .= "$ticker: " . $price . "\n";
+  }
+  else {
+      die "no price found for $ticker";
+  }
+  
 }
 
-# Find longest ticker for alignment
-my $max_length = 0;
-for my $t (@tickers) {
-    $max_length = length($t) if length($t) > $max_length;
+sub update_cache() {
+  foreach my $ticker (@tickers) {
+    cache_prices($ticker);
+  }
+
+  open(my $fh_writer, '>', $cache_file)
+    or die "Can't open $cache_file $!";
+
+  # error handling here ensures data is immediately flushed
+  print $fh_writer $cache
+    or die "Can't write to $cache_file: $!";
+
+  close $fh_writer
 }
 
-# Fixed width for price column ($xx.xx)
-my $price_width = 8;  # room for higher prices
+sub print_cache() {
 
-my $update_only = 0;
+  open(my $fh_reader, '<', $cache_file) 
+    or die "Can't open $cache_file $!";
+
+  print $orange;  # start orange color
+  while (my $line = <$fh_reader>) {  # Angle brackets read a line
+      print "\t$line";
+  }
+  print $reset;  # reset color at end
+
+  close $fh_reader;
+}
+
+#-----------------------------------------------------------------------
+
+
+#-------------------------------- Main ---------------------------------
+
 if (@ARGV && $ARGV[0] eq '--update-only') {
-    $update_only = 1;
+  update_cache();
+  exit;
 }
+else { print_cache(); }
 
-my $ua = Mojo::UserAgent->new;
-$ua->inactivity_timeout(10);
-$ua->max_connections(30);
-$ua->transactor->name('Mozilla/5.0');
-
-sub update_cache {
-    my %new_prices;
-
-    my @promises;
-
-    for my $ticker (@tickers) {
-        my $symbol = lc($ticker) . ".us";
-        my $url    = "https://stooq.pl/q/?s=$symbol";
-
-        my $p = $ua->get_p($url)->then(sub {
-            my ($tx) = @_;
-            my $price = "";
-
-            if ($tx->success) {
-                my $page = $tx->res->body;
-                if ($page =~ /id=aq_\Q$symbol\E_c4>(\d+\.\d+)/) {
-                    $price = $1;
-                }
-            }
-
-            if ($price ne "") {
-                $new_prices{$ticker} = sprintf "%.2f", $price;
-            }
-        })->catch(sub {
-            warn "Failed to fetch $ticker: @_\n" if 0;  # set to 1 for debug
-        });
-
-        push @promises, $p;
-    }
-
-    # block until all requests are done
-    Mojo::Promise->all(@promises)->wait;
-
-    # write updated cache
-    open my $fh, '>', $cache_file;
-    for my $tick (@tickers) {
-        my $pr = $new_prices{$tick} // $cache{$tick} // '(no price)';
-        print $fh "$tick:$pr\n";
-    }
-    close $fh;
-}
-
-if ($update_only) {
-    update_cache();
-    exit;
-}
-
-# print from cache
-print $orange;  # start orange color
-
-for my $ticker (@tickers) {
-    my $price = $cache{$ticker} // "";
-    if ($price eq "") {
-        printf "\t%-*s: (no price)\n", $max_length, $ticker;
-        next;
-    }
-
-    my $price_part = "\$$price";
-
-    printf "\t%-*s: %*s\n",
-        $max_length, $ticker,
-        $price_width, $price_part;
-}
-
-print $reset;  # reset color at end
-
-# Update in background
-if (my $pid = fork) {
-    # Parent
-} elsif (defined $pid) {
-    # Child
-    update_cache();
-    exit;
-} else {
-    warn "Fork failed: $!\n";
-}
+#-----------------------------------------------------------------------
